bb9fd4fec3131e0d04b8b647102682f79dc39d53
Chris <30566151+CJcool06@users.noreply.github.com>
2023-06-06 11:36:08 +0930

    Dir

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 cd mask2former/modeling/pixel_decoder/ops
 python3 setup.py build install
 cd ../../../../
-python train_net.py --num-gpus 4 --config-file configs/coco/panoptic-segmentation/maskformer2_R50_bs16_50ep.yaml OUTPUT_DIR ~/data/detectron2_training_output
+python train_net.py --num-gpus 4 --config-file configs/coco/panoptic-segmentation/maskformer2_R50_bs16_50ep.yaml OUTPUT_DIR /data/detectron2_training_output
